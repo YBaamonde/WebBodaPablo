5aefc40ed58faf1b61ac450e8e5dea9675d3b9b1
YBaamonde <yagobaamonde@gmail.com>
2026-03-15 13:45:50 +0100

    Colores y fuentes

--- js/images.js
+++ js/images.js
@@ -1,68 +1,49 @@
 // Generado automáticamente — no editar a mano
 export const IMAGES = [
   {
-    "file": "foto-01.jpg",
-    "position": "50% 95%"
+    file: "foto-24.jpg",
+    position: "50% 10%",
+  },
+  {
+    file: "foto-01.jpg",
+    position: "50% 95%",
   },
   "foto-02.jpg",
   "foto-03.jpg",
   "foto-04.jpg",
-  "foto-05.jpg",
   {
-    "file": "foto-06.jpg",
-    "position": "50% 80%"
-  },
-  {
-    "file": "foto-07.jpg",
-    "position": "50% 80%"
+    file: "foto-07.jpg",
+    position: "50% 80%",
   },
   "foto-08.jpg",
-  {
-    "file": "foto-09.jpg",
-    "position": "50% 20%"
-  },
   "foto-10.jpg",
-  "foto-11.jpg",
-  "foto-12.jpg",
   {
-    "file": "foto-13.jpg",
-    "position": "50% 1%"
+    file: "foto-13.jpg",
+    position: "50% 1%",
   },
   "foto-14.jpg",
   "foto-15.jpg",
   {
-    "file": "foto-16.jpeg",
-    "position": "50% 40%"
-  },
-  {
-    "file": "foto-17.jpeg",
-    "position": "50% 70%"
+    file: "foto-17.jpeg",
+    position: "50% 70%",
   },
   "foto-18.jpeg",
   "foto-19.jpeg",
   {
-    "file": "foto-20.jpeg",
-    "position": "50% 90%"
+    file: "foto-20.jpeg",
+    position: "50% 90%",
   },
   "foto-21.jpg",
   {
-    "file": "foto-22.jpg",
-    "position": "50% 90%"
+    file: "foto-22.jpg",
+    position: "50% 90%",
   },
   {
-    "file": "foto-23.jpg",
-    "position": "50% 70%"
+    file: "foto-23.jpg",
+    position: "50% 70%",
   },
   {
-    "file": "foto-24.jpg",
-    "position": "50% 10%"
+    file: "foto-26.jpeg",
+    position: "50% 70%",
   },
-  {
-    "file": "foto-25.jpeg",
-    "position": "50% 70%"
-  },
-  {
-    "file": "foto-26.jpeg",
-    "position": "50% 70%"
-  }
 ];
